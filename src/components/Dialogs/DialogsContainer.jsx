b499ff0f35681a8f7645ad2648a9dc0c08323c39
IlyaKd <ilyakd@protonmail.com>
2021-06-03 19:57:21 +0300

    added context

--- src/components/Dialogs/DialogsContainer.jsx
+++ src/components/Dialogs/DialogsContainer.jsx
@@ -1,22 +1,28 @@
 import React from 'react';
 import Dialogs from './Dialogs';
 import { sendMessageActionCreator, updateNewMessageBodyActionCreator } from '../../redux/dialogs-reducer';
+import StoreContext from '../../StoreContext';
 
-const DialogsContainer = (props) => {
-
-    let state = props.store.getState().dialogsPage;
-
-    let onSendMessageClick = () => {
-        props.store.dispatch(sendMessageActionCreator());
-    }
-    let onNewMessageChange = (body) => {
-        props.store.dispatch(updateNewMessageBodyActionCreator(body));
-    }
+const DialogsContainer = () => {
 
     return (
-        <Dialogs updateNewMessageBody={onNewMessageChange} 
-                 sendMessage={onSendMessageClick}
-                 dialogsPage={state} />
+        <StoreContext.Consumer> 
+            {(store) => {
+
+                let state = store.getState().dialogsPage;
+
+                let onSendMessageClick = () => {
+                    store.dispatch(sendMessageActionCreator());
+                }
+                let onNewMessageChange = (body) => {
+                    store.dispatch(updateNewMessageBodyActionCreator(body));
+                }
+
+                return <Dialogs updateNewMessageBody={onNewMessageChange} 
+                                sendMessage={onSendMessageClick}
+                                dialogsPage={state} />
+            }}
+        </StoreContext.Consumer>
     );
 }
 
