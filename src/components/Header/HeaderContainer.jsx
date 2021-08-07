d1eed503912523c4cef95d841c40aa762566fb64
IlyaKd <ilyakd@protonmail.com>
2021-08-07 12:34:07 +0300

    added hoc

--- src/components/Header/HeaderContainer.jsx
+++ src/components/Header/HeaderContainer.jsx
@@ -1,23 +1,12 @@
 import React from 'react';
-/*import axios from 'axios';*/
 import Header from './Header';
 import { connect } from 'react-redux';
-import { /*setAuthUserData,*/ getAuthUserData } from '../../redux/auth-reducer'
+import { getAuthUserData } from '../../redux/auth-reducer'
 
 class HeaderContainer extends React.Component {
 
     componentDidMount() {
         this.props.getAuthUserData();
-
-        /*axios.get('https://social-network.samuraijs.com/api/1.0/auth/me', {
-            withCredentials: true
-        })
-            .then(response => {
-                if (response.data.resultCode === 0) {
-                    let {id, login, email} = response.data.data;
-                    this.props.setAuthUserData(id, email, login);
-                }
-            })*/
     }
     
     render () {
@@ -30,4 +19,4 @@ const mapStateToProps = (state) => ({
     login: state.auth.login,
 });
 
-export default connect(mapStateToProps, {/*setAuthUserData,*/ getAuthUserData}) (HeaderContainer);
+export default connect(mapStateToProps, {getAuthUserData}) (HeaderContainer);
