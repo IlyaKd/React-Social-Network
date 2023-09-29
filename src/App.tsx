7dce5b3b101dc244dd3aec1dc9dac1c589d079f7
IlyaKd <ilyakd@protonmail.com>
2023-09-29 17:56:30 +0000

    AntDesign

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import './App.css';
-import HeaderContainer from './components/Header/HeaderContainer';
-import Navbar from './components/Navbar/Navbar';
-import {BrowserRouter, Redirect, Route, Switch, withRouter} from 'react-router-dom';
+import {BrowserRouter, Link, Redirect, Route, Switch, withRouter} from 'react-router-dom';
 import { connect, Provider } from 'react-redux';
 import { initializeApp } from './redux/app-reducer';
 import { compose } from 'redux';
@@ -11,6 +9,12 @@ import store, { AppStateType } from './redux/redux-store';
 import {withSuspense} from "./hoc/withSuspense";
 import { UsersPage } from './components/Users/UsersPage';
 import { LoginPage } from './components/Login/LoginPage';
+import { Breadcrumb, Layout, Menu } from 'antd';
+import { LaptopOutlined, NotificationOutlined, UserOutlined } from '@ant-design/icons';
+import { Header } from './components/Header/Header';
+
+const { Footer, Sider, Content } = Layout;
+const {SubMenu} = Menu;
 
 const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'));
 const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainer'));
@@ -40,34 +44,100 @@ class App extends React.Component<MapPropsType & DispatchPropsType> {
     }
 
     return (
-      // <BrowserRouter>
-        <div className='app-wrapper'>
-          <HeaderContainer />
-          <Navbar />
-          <div className='app-wrapper-content'>
-            <Switch>
-              <Route exact path='/'
-                render={() => <Redirect to={"/profile"} />} />
+      <Layout>
+          <Header />
+          <Content style={{padding: '0 50px'}}>
+              <Breadcrumb style={{margin: '16px 0'}}>
+                  <Breadcrumb.Item>Home</Breadcrumb.Item>
+                  <Breadcrumb.Item>List</Breadcrumb.Item>
+                  <Breadcrumb.Item>App</Breadcrumb.Item>
+              </Breadcrumb>
+              <Layout className="site-layout-background" style={{padding: '24px 0'}}>
+                  <Sider className="site-layout-background" width={200}>
+                      <Menu
+                          mode="inline"
+                          /*  defaultSelectedKeys={['7']}*/
+                          /*  defaultOpenKeys={['sub1']}*/
+                          style={{height: '100%'}}
+                      >
+                          <SubMenu key="sub1" icon={<UserOutlined/>} title="My Profile">
+                              <Menu.Item key="1"> <Link to="/profile">Profile</Link></Menu.Item>
+                              <Menu.Item key="2"> <Link to="/dialogs">Messages</Link></Menu.Item>
+                              <Menu.Item key="3">option3</Menu.Item>
+                              <Menu.Item key="4">option4</Menu.Item>
+                          </SubMenu>
+                          <SubMenu key="sub2" icon={<LaptopOutlined/>} title="Developers">
+                              <Menu.Item key="5"><Link to="/developers">Developers</Link></Menu.Item>
+                              <Menu.Item key="6">option6</Menu.Item>
+                              <Menu.Item key="7">option7</Menu.Item>
+                              <Menu.Item key="8">option8</Menu.Item>
+                          </SubMenu>
+                          <SubMenu key="sub3" icon={<NotificationOutlined/>} title="subnav 3">
+                              <Menu.Item key="9">option9</Menu.Item>
+                              <Menu.Item key="10">option10</Menu.Item>
+                              <Menu.Item key="11">option11</Menu.Item>
+                              <Menu.Item key="12">option12</Menu.Item>
+                          </SubMenu>
+                      </Menu>
+                  </Sider>
+                  <Content style={{padding: '0 24px', minHeight: 280}}>
 
-              <Route path='/dialogs'
-                render={() => <SuspendedDialogs />} />
+                      <Switch>
+                          <Route exact path='/'
+                                 render={() => <Redirect to={'/profile'}/>}/>
 
-              <Route path='/profile/:userId?'
-                render={() => <SuspendedProfile />} />
+                          <Route path='/dialogs'
+                                 render={() => <SuspendedDialogs/>}/>
 
-              <Route path='/users'
-                render={() => <UsersPage pageTitle={"Самураи"} />} />
+                          <Route path='/profile/:userId?'
+                                 render={() => <SuspendedProfile/>}/>
 
-              <Route path='/login'
-                render={() => <LoginPage />} />
+                          <Route path='/developers'
+                                 render={() => <UsersPage pageTitle={'Самураи'}/>}/>
 
-              <Route path='*'
-                render={() => <div>404 NOT FOUND</div>} />
-            </Switch>
-          </div>
-        </div>
-      // </BrowserRouter>
-    );
+                          <Route path='/login'
+                                 render={() => <LoginPage/>}/>
+
+                          <Route path='*'
+                                 render={() => <div>404 NOT FOUND</div>}/>
+                      </Switch>
+
+                  </Content>
+              </Layout>
+          </Content>
+          <Footer style={{textAlign: 'center'}}>Samurai Social Network ©2020 Created by IT-KAMASUTRA</Footer>
+      </Layout>
+    )
+
+    // return (
+    //   // <BrowserRouter>
+    //     <div className='app-wrapper'>
+    //       <HeaderContainer />
+    //       <Navbar />
+    //       <div className='app-wrapper-content'>
+    //         <Switch>
+    //           <Route exact path='/'
+    //             render={() => <Redirect to={"/profile"} />} />
+
+    //           <Route path='/dialogs'
+    //             render={() => <SuspendedDialogs />} />
+
+    //           <Route path='/profile/:userId?'
+    //             render={() => <SuspendedProfile />} />
+
+    //           <Route path='/users'
+    //             render={() => <UsersPage pageTitle={"Самураи"} />} />
+
+    //           <Route path='/login'
+    //             render={() => <LoginPage />} />
+
+    //           <Route path='*'
+    //             render={() => <div>404 NOT FOUND</div>} />
+    //         </Switch>
+    //       </div>
+    //     </div>
+    //   // </BrowserRouter>
+    // );
   }
 }
 
